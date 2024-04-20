fae8aca9acc2a17a77613fbc731b9ac3d489db31
Lopez-Rojas Nathan-Raphael <nathan-raphael.lopez-rojas@etu.umontpellier.fr>
2024-04-20 22:26:47 +0200

    Ajout de la méthode donnePvBonus dans Joueur.java qui a pour but d'implémenter de nouveaux points bonus grâce aux effets de la carte utilisé.

--- src/main/java/fr/umontpellier/iut/trains/cartes/Appartement.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/Appartement.java
@@ -1,7 +1,15 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Joueur;
+
 public class Appartement extends Carte {
     public Appartement() {
         super("Appartement", 0, 3, Type.VICTOIRE, "Lorsque vous achetez cette carte : Recevez une carte féraille.", 1);
+    }
+
+    @Override
+    public void jouer(Joueur joueur) {
+        joueur.donnePvBonus(super.getPv());
+        //Pas terminé, doit faire piocher une carte feraille.
     }
 }
