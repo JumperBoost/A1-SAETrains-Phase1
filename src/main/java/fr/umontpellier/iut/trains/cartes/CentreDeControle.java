c863a648a799df70a64d508e551e2795209bfe9b
Lopez-Rojas Nathan-Raphael <nathan-raphael.lopez-rojas@etu.umontpellier.fr>
2024-05-04 00:02:07 +0200

    Ajout des boutons à afficher durant l'utilisation de CentreDeControle. Peut être modifié si possible

--- src/main/java/fr/umontpellier/iut/trains/cartes/CentreDeControle.java
+++ src/main/java/fr/umontpellier/iut/trains/cartes/CentreDeControle.java
@@ -1,6 +1,9 @@
 package fr.umontpellier.iut.trains.cartes;
 
+import fr.umontpellier.iut.trains.Bouton;
 import fr.umontpellier.iut.trains.Joueur;
+
+import java.util.ArrayList;
 
 public class CentreDeControle extends Carte {
     public CentreDeControle() {
@@ -12,12 +15,15 @@ public class CentreDeControle extends Carte {
         super.jouer(joueur);
         joueur.setCarteAction(this);
         if(joueur.getPeutPiocher()) {
+            ArrayList<Bouton> boutonAjouter = new ArrayList<>();
             joueur.setPeutPasser(false);
             joueur.getMain().add(joueur.piocher());
             for (Carte carte : joueur.getCartes())
                 joueur.ajouterChoixPossibleAction(carte.getNom());
-            for (String nomCarte : joueur.getJeu().getListeNomsCartes())
+            for (String nomCarte : joueur.getJeu().getListeNomsCartes()){
                 joueur.ajouterChoixPossibleAction(nomCarte);
+                boutonAjouter.add(new Bouton(nomCarte));
+            }
             for (Carte carte : joueur.getJeu().getCartesEcartees())
                 joueur.ajouterChoixPossibleAction(carte.getNom());
         }
